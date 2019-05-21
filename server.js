7ab992d8a5874fa28b492e3ef6a0e1e337039798
Yaltha <rullistyaltha@gmail.com>
2019-05-21 16:03:00 +0700

    Until-ejs

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
 
+const indexRouter = require("./routes/index");
+
 app.set("view engine", "ejs"); // ejs for viewer
-app.set("view", __dirname + "/views"); //all the file are coming from that to be viewed
+app.set("views", __dirname + "/views"); //all the file are coming from that to be viewed
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
+
+app.use("/", indexRouter);
 
 app.listen(process.env.PORT || 3000, () =>
   console.log("Server's Listening...!")
